36b31961e8990a8c72404b058f362fefe14ee667
Nicholas Reynolds <sonshine_80@yahoo.com>
2015-11-17 15:28:38 -0800

    Finishes assignment from yesterday and starts working on the clicks in react

---
+++ js/components/detail.jsx
@@ -0,0 +1,18 @@
+var React = require('react');
+
+var Detail = React.createClass({
+  render: function() {
+    return (
+      <div className="detail">
+        <img src= {this.props.data.url} />
+          <ul>
+          <li><strong>Title:</strong> {this.props.data.title}</li>
+          <li><strong>Description:</strong> {this.props.data.description}</li>
+          <li><a href={"/post/" + this.props.data.objectId + "/edit"}>Edit</a></li>
+       </ul>
+      </div>
+    )
+  }
+});
+
+module.exports = Detail;
--- js/router.jsx
+++ js/router.jsx
@@ -1,10 +1,14 @@
 var Backbone = require('./backbone-parse');
 var Post = require('./models/post');
 var Posts = require('./collections/posts');
-var mainTemplate = require('./templates/main.html');
-var detailTemplate = require('./templates/detail.html');
-var addEditTemplate = require('./templates/addEdit.html');
-
+//var mainTemplate = require('./templates/main.html');
+//var detailTemplate = require('./templates/detail.html');
+//var addEditTemplate = require('./templates/addEdit.html');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Main = require('./components/main.jsx');
+var Detail = require('./components/detail.jsx');
+var AddEdit= require('./components/edit.jsx');
 var Router = Backbone.Router.extend({
   initialize: function () {
     Backbone.history.start({pushState: true});
@@ -18,8 +22,11 @@ var Router = Backbone.Router.extend({
   index: function () {
     Posts.fetch({
       success: function (posts) {
-        var html = mainTemplate({'data': posts.toJSON()});
-        $("#container").html(html);
+        // var html = mainTemplate({'data': posts.toJSON()});
+        // $("#container").html(html);
+        var data = posts.toJSON();
+
+        ReactDOM.render(<Main data={data}/>, document.getElementById("container"));
       }
     });
   }
@@ -29,19 +36,25 @@ var router = new Router();
 
 router.on('route:post', function (objectId) {
   var post = Posts.get(objectId);
-  var html = detailTemplate(post.toJSON());
-  $("#container").html(html);
+  // var html = detailTemplate(post.toJSON());
+  // $("#container").html(html);
+  var data = post.toJSON();
+  ReactDOM.render(<Detail data={data}/>, document.getElementById("container"));
 });
 
 router.on('route:add', function () {
-  var html = addEditTemplate({});
-  $("#container").html(html);
+  //var html = addEditTemplate({});
+  //$("#container").html(html);
+  // var data = post.toJSON();
+  ReactDOM.render(<AddEdit/>, document.getElementById("container"));
 });
 
 router.on('route:edit', function (objectId) {
   var post = Posts.get(objectId);
-  var html = addEditTemplate(post.toJSON());
-  $("#container").html(html);
+  // var html = addEditTemplate(post.toJSON());
+  // $("#container").html(html);
+  var data = post.toJSON();
+  ReactDOM.render(<AddEdit data={data}/>, document.getElementById("container"));
 });
 
 $('body').on('click', 'a', function (e){
@@ -79,3 +92,4 @@ $("body").on('submit', "#detailForm", function (e) {
 });
 
 module.exports = router;
+
