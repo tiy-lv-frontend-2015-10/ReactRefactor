73af50dfd65c388a0dfe4a719a0df7b0adabe2c1
noemiostorga <noemiostorga@gmail.com>
2015-11-18 07:57:23 -0800

    button

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ require("../css/main.css");
 var React = require('react');
 var ReactDOM = require ('react-dom');
 
-// Only uncomment this if you're wanting to generate more data
+//// Only uncomment this if you're wanting to generate more data
 //var data = require('./generateData');
 //data.generate(15);
 $(document).ready(function(){
